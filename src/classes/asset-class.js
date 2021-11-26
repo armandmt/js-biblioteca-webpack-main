4f64af79846a90eeac93339c8edcc7dd7aa692ab
Armand Mata <amata3@xtec.cat>
2021-11-26 17:05:59 +0100

    aaaa

---
+++ src/classes/asset-class.js
@@ -0,0 +1,19 @@
+export class Asset {
+
+
+constructor(id,username,password,role_id) {
+
+
+    this.id = id;
+    this.model = model;
+    this.location = location;
+
+} 
+
+}
+
+id: integer
+model_id: integer
+location_id: integer
+created: datetime
+updated: datetime
