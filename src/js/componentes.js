a1ed689af837a247501205937b1203eb4bc8a456
armandmt <armandmt@gmail.com>
2021-11-30 12:12:53 +0100

    3011

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -21,33 +21,39 @@ export const isLogged = () => {
 }
 
 
-export const creaHTMLlistaLlibres = (llistallibres, llistaautors,llistaeditorials,llistageneres) =>{
+// export const creaHTMLlistaLlibres = (llistallibres, llistaautors,llistaeditorials,llistageneres) =>{
 
-    let html=`<table class='table'>
-    <thead><tr><th>#Id</th><th>Llibres</th><th>Autors</th><th>Gènere</th>
-    <th>Editorials</th><th>Valoració</th>
-    <th class='esborrar invisible'><button id="esborraritems" type="button" class="btn btn-outline-danger btn-sm">Esborrar</button>
-    </th></tr></thead><tbody>`
+//     let html=`<table class='table'>
+//     <thead><tr><th>#Id</th><th>Llibres</th><th>Autors</th><th>Gènere</th>
+//     <th>Editorials</th><th>Valoració</th>
+//     <th class='esborrar invisible'><button id="esborraritems" type="button" class="btn btn-outline-danger btn-sm">Esborrar</button>
+//     </th></tr></thead><tbody>`
 
-    for (let i of llistallibres.llibres)  {
+    //for (let i of llistallibres.llibres)
+    
+    // for(index=0; index < llistallibres.lilibres.length; index++)
+    // {
    
-        let cad_autors= llistaautors.cercaAutors(i.id_autors)
-        let cad_editorials = llistaeditorials.cercaEditorial(i.id_editorial)
-        let cad_generes = llistageneres.cercaGenere(i.id_genere)
-        html += `<tr ><td>${i.id_llibre}</td>
-        <td>${i.titol}</td><td>${cad_autors}
-        </td><td>${cad_generes} </td><td>${cad_editorials}
-        </td><td>${i.valoracio}</td>
-        <td><input class="esborrar form-check-input invisible" type="checkbox" value="" id="esb${i.id_llibre}""></td></tr>`
-        console.log(i.titol)
+    //     let cad_autors= llistaautors.cercaAutors(i.id_autors)
+    //     let cad_editorials = llistaeditorials.cercaEditorial(i.id_editorial)
+    //     let cad_generes = llistageneres.cercaGenere(i.id_genere)
+    //     html += `<tr ><td>${i.id_llibre}</td>
+    //     <td>${i.titol}</td><td>${cad_autors}
+    //     </td><td>${cad_generes} </td><td>${cad_editorials}
+    //     </td><td>${i.valoracio}</td>
+    //     <td><input class="esborrar form-check-input invisible" type="checkbox" value="" id="esb${i.id_llibre}""></td></tr>`
+    //     console.log(i.titol)
 
 
-    }
-    html +=`</tbody></table>`
-    return html;
+    // }
+    // html +=`</tbody></table>`
+//     return html;
 
 
-}
+    
+    
+
+// }
 
 
 
--- src/vistes/llistaLlibres.js
+++ src/vistes/llistaLlibres.js
@@ -6,20 +6,29 @@ export const creaHTMLlistaLlibres = (llistallibres, llistaautors,llistaeditorial
     <th class='esborrar invisible'><button id="esborraritems" type="button" class="btn btn-outline-danger btn-sm">Esborrar</button>
     </th></tr></thead><tbody>`
 
-    for (let i of llistallibres.llibres)  {
-   
-        let cad_autors= llistaautors.cercaAutors(i.id_autors)
-        let cad_editorials = llistaeditorials.cercaEditorial(i.id_editorial)
-        let cad_generes = llistageneres.cercaGenere(i.id_genere)
-        html += `<tr ><td>${i.id_llibre}</td>
-        <td>${i.titol}</td><td>${cad_autors}
+    
+    llistallibres.llibres.forEach( (v,i,array) => {
+        
+        let cad_autors= llistaautors.cercaAutors(v.id_autors)
+        let cad_editorials = llistaeditorials.cercaEditorial(v.id_editorial)
+        let cad_generes = llistageneres.cercaGenere(v.id_genere)
+        html += `<tr ><td>${v.id_llibre}</td>
+        <td>${v.titol}</td><td>${cad_autors}
         </td><td>${cad_generes} </td><td>${cad_editorials}
-        </td><td>${i.valoracio}</td>
-        <td><input class="esborrar form-check-input invisible" type="checkbox" value="" id="esb${i.id_llibre}""></td></tr>`
-        console.log(i.titol)
+        </td><td>${v.valoracio}</td>
+        <td><input class="esborrar form-check-input invisible" type="checkbox" value="" id="esb${v.id_llibre}""></td></tr>`
+        //console.log(v.titol)
+        
+    });
+    
+   
+
+    
+
+       
 
 
-    }
+    
     html +=`</tbody></table>`
     return html;
 
